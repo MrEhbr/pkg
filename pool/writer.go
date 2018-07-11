aaccb729296e44fe9a4e79ceb8154414c6081b86
Aleksey Burmistrov <aleksey.burmistrov@iqoption.com>
2018-07-11 12:51:19 +0300

    ~

--- pool/writer.go
+++ pool/writer.go
@@ -1,36 +1,38 @@
 package pool
 
 import (
+	"bufio"
 	"bytes"
 )
 
 // BufferPool implements a pool of bytes.Buffers in the form of a bounded channel.
-type BufferPool struct {
-	c chan *bytes.Buffer
+type WriterPool struct {
+	c chan *bufio.Writer
 }
 
 // NewBufferPool creates a new BufferPool bounded to the given size.
-func NewBufferPool(size int) (bp *BufferPool) {
-	return &BufferPool{
-		c: make(chan *bytes.Buffer, size),
+func NewWriterPool(size int) (wp *WriterPool) {
+	return &WriterPool{
+		c: make(chan *bufio.Writer, size),
 	}
 }
 
 // Get gets a Buffer from the BufferPool, or creates a new one if none are available in the pool.
-func (bp *BufferPool) Get() (b *bytes.Buffer) {
+func (wp *WriterPool) Get() (buf *bytes.Buffer, w *bufio.Writer) {
 	select {
-	case b = <-bp.c:
+	case w = <-wp.c:
 	default:
-		b = bytes.NewBuffer([]byte{})
+		buf = bytes.NewBuffer([]byte{})
+		w = bufio.NewWriter(buf)
 	}
 	return
 }
 
 // Put returns the given Buffer to the BufferPool.
-func (bp *BufferPool) Put(b *bytes.Buffer) {
-	b.Reset()
+func (wp *WriterPool) Put(b *bufio.Writer) {
+	b.Reset(b)
 	select {
-	case bp.c <- b:
+	case wp.c <- b:
 	default:
 	}
 }
